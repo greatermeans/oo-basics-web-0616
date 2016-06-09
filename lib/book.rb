db4ef97b55206f9628d8c5b63d6b0dce09e28e37
greatermeans <gakouri@gmail.com>
2016-06-09 17:01:52 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,13 @@
-class Book
+class Book  
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title=nil)
+    @title = title
+    @current_page = 0
+  end
+
+  def turn_page
+    @current_page += 1
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
